e9b9daf4a2391aa2dee013e5dfba906ce69820f5
AbdullaK123 <abdullamaseeh123@gmail.com>
2025-07-20 19:50:10 +0400

    Added logging layer

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod controllers;
 
 use anyhow::Result;
 use tokio::net::TcpListener;
+use tower_http::trace::TraceLayer;
 use crate::config::database::create_pool;
 use crate::controllers::init_app;
 use crate::services::TicketService;
@@ -15,17 +16,25 @@ use crate::services::TicketService;
 async fn main() -> Result<()> {
     dotenvy::dotenv().ok();
 
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
     let pool = create_pool().await?;
 
     let ticket_service = TicketService::new(pool);
 
     let listener = TcpListener::bind("localhost:3000").await?;
 
-    let app = init_app().with_state(ticket_service);
+    let app = 
+        
+        init_app()
+            .layer(TraceLayer::new_for_http())
+            .with_state(ticket_service);
 
     axum::serve(listener, app).await?;
 
-    println!("🚀 Server running on http://localhost:3000");
+    tracing::info!("🚀 Server running on http://localhost:3000");
 
     Ok(())
 }
